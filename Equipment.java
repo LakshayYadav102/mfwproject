ddab6f94ae0e5247ace73053584cde0b7cfb2d82
LakshayYadav102 <141443288+LakshayYadav102@users.noreply.github.com>
2024-03-16 17:09:41 +0530

    Create Equipment.java

---
+++ Equipment.java
@@ -0,0 +1,30 @@
+package com.lakshay.model;
+
+public class Equipment {
+    private Long id;
+    private String name;
+    private String availabilityStatus;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvailabilityStatus() {
+        return availabilityStatus;
+    }
+
+    public void setAvailabilityStatus(String availabilityStatus) {
+        this.availabilityStatus = availabilityStatus;
+    }
+}
